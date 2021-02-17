9ebbac09f7491e80b4287789ae2c98d566ec226b
Lukasz Mazurowski <lukasz.mazurowski@gmail.com>
2021-02-17 10:51:16 +0100

    Updates

---
+++ precache-manifest.a6184da1e2889840025d15f3a6b8289a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c19c7ab0996763c94b86bdedc2f513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "343ee94df8bd0da4e183",
+    "url": "/static/css/main.e7236ad3.chunk.css"
+  },
+  {
+    "revision": "0fb2e4af47df26079f64",
+    "url": "/static/js/2.c7ee543c.chunk.js"
+  },
+  {
+    "revision": "343ee94df8bd0da4e183",
+    "url": "/static/js/main.56687558.chunk.js"
+  },
+  {
+    "revision": "aa1d0f1c672be7083a94",
+    "url": "/static/js/runtime~main.afeea630.js"
+  },
+  {
+    "revision": "4c320c3ff4b253a9490cd69fadf3f34a",
+    "url": "/static/media/avatar.4c320c3f.jpg"
+  },
+  {
+    "revision": "72295c770f9f6c5e4ef1f3f41ed09907",
+    "url": "/static/media/smoke.72295c77.webm"
+  }
+]);
